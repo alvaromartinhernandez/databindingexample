58281e06fd045e6499d343352c9478c37b54cd42
Alvaro Martin <alvaro.martin@acct.global>
2022-06-13 20:10:38 +0200

    apidatabase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  useState,
+  useEffect /*, createContext, useContext*/,
+} from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [peliculas, setPeliculas] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    fetch("http://localhost:3000/peliculas")
+      .then((response) => response.json())
+      .then((peliculas) => {
+        console.log(peliculas);
+        setPeliculas(peliculas); // ⬅️ Guardar datos
+        setIsLoading(false); // ⬅️ Desactivar modo "cargando"
+      });
+  }, []);
+
+  if (isLoading) {
+    // ⬅️ si está cargando, mostramos un texto que lo indique
+    return (
+      <div className="App">
+        <h1>Cargando...</h1>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/*<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +42,18 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header>*/}
+      <table>
+        <tbody>
+          {peliculas?.map((pelicula) => {
+            return (
+              <tr key={pelicula.id}>
+                <td>{pelicula.nombre}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
